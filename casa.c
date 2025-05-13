f87a50c5612a09003f93006400c8e709bd872cb9
gusDev <joaogustavolima084@gmail.com>
2025-05-13 15:23:23 -0300

    reflexão

--- casa.c
+++ casa.c
@@ -11,12 +11,14 @@
 
 unsigned char img[IMAGE_HEIGHT][IMAGE_WIDTH][3];
 
-typedef struct{
+typedef struct
+{
     int x;
     int y;
 } Point;
 
-typedef struct{
+typedef struct
+{
     Point start;
     Point end;
     unsigned char r, g, b;
@@ -34,8 +36,10 @@ void setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b);
 void drawLine(Point start, Point end, unsigned char r, unsigned char g, unsigned char b);
 void reflectPoint(Point *p, int reflectX, int reflectY, Point center);
 void reflectHouse(Line house[NUM_HOUSE_LINES], int reflectX, int reflectY);
+void espelharCasas(Line original[NUM_HOUSE_LINES], int refletirX, int refletirY, int ambos);
 
-int main(){
+int main()
+{
     const float scale_factor = 1;
     const float rotation_angle = 0.0;
 
@@ -47,28 +51,18 @@ int main(){
     clearImage();
     drawHouse(house);
 
-    Line reflectedX[NUM_HOUSE_LINES];
-    Line reflectedY[NUM_HOUSE_LINES];
-    Line reflectedXY[NUM_HOUSE_LINES];
-
-    memcpy(reflectedX, house, sizeof(house));
-    memcpy(reflectedY, house, sizeof(house));
-    memcpy(reflectedXY, house, sizeof(house));
-
-    reflectHouse(reflectedX, 1, 0);  // Reflexão no eixo X (abaixo)
-    reflectHouse(reflectedY, 0, 1);  // Reflexão no eixo Y (esquerda)
-    reflectHouse(reflectedXY, 1, 1); // Reflexão nos dois eixos (diagonal)
-
-    drawHouse(reflectedX);
-    drawHouse(reflectedY);
-    drawHouse(reflectedXY);
+    // Personalize os parâmetros abaixo:
+    // refletirX = 1 ativa espelhamento no eixo X
+    // refletirY = 1 ativa espelhamento no eixo Y
+    // ambos = 1 ativa espelhamento nos dois eixos
+    espelharCasas(house, 0, 0, 0); // exemplo: espelhar nos 3 lados
 
     saveImage();
-
     return 0;
 }
 
-void clampPoint(Point *p){
+void clampPoint(Point *p)
+{
     if (p->x < 0)
         p->x = 0;
     if (p->x >= IMAGE_WIDTH)
@@ -81,15 +75,12 @@ void clampPoint(Point *p){
 
 void scalePoint(Point *p, Point center, float scale)
 {
-    // Calcula a escala máxima permitida para caber na imagem
     float max_scale_x = fminf(
         (center.x) / fabsf(p->x - center.x),
         (IMAGE_WIDTH - center.x) / fabsf(p->x - center.x));
-
     float max_scale_y = fminf(
         (center.y) / fabsf(p->y - center.y),
         (IMAGE_HEIGHT - center.y) / fabsf(p->y - center.y));
-
     float safe_scale = fminf(fminf(max_scale_x, max_scale_y), scale);
 
     float translatedX = p->x - center.x;
@@ -104,7 +95,8 @@ void scalePoint(Point *p, Point center, float scale)
     clampPoint(p);
 }
 
-void rotatePoint(Point *p, Point center, float angle_degrees){
+void rotatePoint(Point *p, Point center, float angle_degrees)
+{
     float angle_rad = angle_degrees * M_PI / 180.0f;
     float cos_theta = cosf(angle_rad);
     float sin_theta = sinf(angle_rad);
@@ -124,11 +116,9 @@ void transformHouse(Line house[NUM_HOUSE_LINES], float scale, float angle)
 
     for (int i = 0; i < NUM_HOUSE_LINES; i++)
     {
-        // Aplica rotação primeiro
         rotatePoint(&house[i].start, center, angle);
         rotatePoint(&house[i].end, center, angle);
 
-        // Depois escala
         scalePoint(&house[i].start, center, scale);
         scalePoint(&house[i].end, center, scale);
     }
@@ -152,7 +142,7 @@ void reflectPoint(Point *p, int reflectX, int reflectY, Point center)
 
 void reflectHouse(Line house[NUM_HOUSE_LINES], int reflectX, int reflectY)
 {
-    const Point center = {IMAGE_WIDTH / 2, IMAGE_HEIGHT / 2}; // <- centro da imagem
+    const Point center = {IMAGE_WIDTH / 2, IMAGE_HEIGHT / 2};
     for (int i = 0; i < NUM_HOUSE_LINES; i++)
     {
         reflectPoint(&house[i].start, reflectX, reflectY, center);
@@ -160,7 +150,35 @@ void reflectHouse(Line house[NUM_HOUSE_LINES], int reflectX, int reflectY)
     }
 }
 
-void initializeHouse(Line house[NUM_HOUSE_LINES]){
+void espelharCasas(Line original[NUM_HOUSE_LINES], int refletirX, int refletirY, int ambos)
+{
+    if (refletirX)
+    {
+        Line casaX[NUM_HOUSE_LINES];
+        memcpy(casaX, original, sizeof(Line) * NUM_HOUSE_LINES);
+        reflectHouse(casaX, 1, 0);
+        drawHouse(casaX);
+    }
+
+    if (refletirY)
+    {
+        Line casaY[NUM_HOUSE_LINES];
+        memcpy(casaY, original, sizeof(Line) * NUM_HOUSE_LINES);
+        reflectHouse(casaY, 0, 1);
+        drawHouse(casaY);
+    }
+
+    if (ambos)
+    {
+        Line casaXY[NUM_HOUSE_LINES];
+        memcpy(casaXY, original, sizeof(Line) * NUM_HOUSE_LINES);
+        reflectHouse(casaXY, 1, 1);
+        drawHouse(casaXY);
+    }
+}
+
+void initializeHouse(Line house[NUM_HOUSE_LINES])
+{
     house[0] = (Line){{0, 64}, {64, 0}, 255, 0, 0};
     house[1] = (Line){{64, 0}, {128, 64}, 255, 0, 0};
     house[2] = (Line){{0, 64}, {128, 64}, 0, 0, 255};
@@ -171,17 +189,17 @@ void initializeHouse(Line house[NUM_HOUSE_LINES]){
     house[7] = (Line){{80, 128}, {80, 98}, 255, 255, 0};
     house[8] = (Line){{48, 98}, {80, 98}, 255, 255, 0};
 
-    int door_width = 32;  // Largura da porta
-    int door_height = 30; // Altura da porta
-    int base_y = 128;     // Base da casa
+    int door_width = 32;
+    int door_height = 30;
+    int base_y = 128;
 
-    // Pontos da porta
-    house[6] = (Line){{(128 - door_width) / 2, base_y}, {(128 - door_width) / 2, base_y - door_height}, 255, 255, 0}; // Esquerda
-    house[7] = (Line){{(128 + door_width) / 2, base_y}, {(128 + door_width) / 2, base_y - door_height}, 255, 255, 0}; // Direita
+    house[6] = (Line){{(128 - door_width) / 2, base_y}, {(128 - door_width) / 2, base_y - door_height}, 255, 255, 0};
+    house[7] = (Line){{(128 + door_width) / 2, base_y}, {(128 + door_width) / 2, base_y - door_height}, 255, 255, 0};
     house[8] = (Line){{(128 - door_width) / 2, base_y - door_height}, {(128 + door_width) / 2, base_y - door_height}, 255, 255, 0};
 }
 
-void setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b){
+void setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b)
+{
     if (x >= 0 && x < IMAGE_WIDTH && y >= 0 && y < IMAGE_HEIGHT)
     {
         img[y][x][0] = r;
@@ -190,7 +208,8 @@ void setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b){
     }
 }
 
-void drawLine(Point start, Point end, unsigned char r, unsigned char g, unsigned char b){
+void drawLine(Point start, Point end, unsigned char r, unsigned char g, unsigned char b)
+{
     for (float t = 0.0; t <= 1.0; t += 0.0001)
     {
         int x = start.x + t * (end.x - start.x);
@@ -199,21 +218,24 @@ void drawLine(Point start, Point end, unsigned char r, unsigned char g, unsigned
     }
 }
 
-void drawHouse(Line house[NUM_HOUSE_LINES]){
+void drawHouse(Line house[NUM_HOUSE_LINES])
+{
     for (int i = 0; i < NUM_HOUSE_LINES; i++)
     {
         drawLine(house[i].start, house[i].end, house[i].r, house[i].g, house[i].b);
     }
 }
 
-void clearImage(){
+void clearImage()
+{
     for (int i = 0; i < IMAGE_HEIGHT; i++)
         for (int j = 0; j < IMAGE_WIDTH; j++)
             for (int c = 0; c < 3; c++)
                 img[i][j][c] = 0;
 }
 
-void saveImage(){
+void saveImage()
+{
     printf("P3\n%d %d\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
     for (int i = 0; i < IMAGE_HEIGHT; i++)
     {
